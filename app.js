4536bba545bc315b0b2077176281b8d346e5e530
Henry Gabriel <106675861+henryornellas@users.noreply.github.com>
2023-03-29 20:35:21 -0300

    Fix google auth

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://authentication-rp1h.onrender.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
